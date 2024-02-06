65959d5597a17f69dde590e91df0bc99c82bf93d
swesharkie <swe.sharkie@gmail.com>
2024-02-06 21:32:52 +0100

    New version! 19.02.39

--- pull_revanced.sh
+++ pull_revanced.sh
@@ -3,9 +3,13 @@ url=$(curl -s "https://api.github.com/repos/NoName-exe/revanced-extended/release
 filename=$(echo $url | sed -nr 's/.*(youtube-revanced-extended-v.*\.apk)/\1/p')
 if grep -Fxq "$filename" downloaded.txt
 then
-    echo "true"
+    echo "nothing new"
 else
     curl -LO $url
     echo $filename >> downloaded.txt
     find . -type f -name "youtube-revanced-extended-v*.apk" -execdir bash -c 'mv "$0" "youtube-revanced-extended-latest.apk"' {} \;
+    version=$(echo $filename | sed -nr 's/.*v(.*)-all\.apk/\1/p')
+    git add -u
+    git commit -m "New version! $version"
+    git push
 fi
